91faf079dccfe7db5caaae10ea6ebe10cf61829b
Mario Monteiro <marioammonteiro@gmail.com>
2021-09-14 11:54:04 +0100

    working on consumer app

---
+++ app-lib/src/auth/user-service.interface.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export interface UserService {
+  find: (id: string) => User;
+  insert: (user: Exclude<User, 'id'>) => User;
+}
---
+++ app-lib/src/index.ts
@@ -0,0 +1,4 @@
+export * from './auth/auth.constants';
+export * from './auth/auth.interface';
+export * from './auth/auth.module';
+export * from './auth/auth.service';
